1d738e2e54136498a8e9808cdbe0fa2e281da060
Josh Hobson <joshuahobson1995@gmail.com>
2023-05-23 14:59:49 -0600

    seminar fixed for mobile

--- src/components/Seminar/Seminars.js
+++ src/components/Seminar/Seminars.js
@@ -8,32 +8,30 @@ import seminar_pic from '../../Assets/seminar_pic.jpg'
 
 function Seminars() {
 
-    const revealRef = useRef(null); 
-    const [isVisible, setIsVisible] = useState(false);
+  const revealRef1 = useRef(null); 
+  const [isAnimateds, setIsAnimateds] = useState(false); 
 
-    const handleScroll = () => {
-      const revealElement = revealRef.current; 
-      if (revealElement) {
-        const revealTop = revealElement.getBoundingClientRect().top; 
-        const windowHeight = window.innerHeight; 
+  const handleScroll = () => {
+    const revealElement = revealRef1.current; 
+    if (revealElement) {
+      const revealTop = revealElement.getBoundingClientRect().top; 
+      const windowHeight = window.innerHeight; 
 
-        if (revealTop < windowHeight) {
-            setTimeout(() => {
-            setIsVisible(true);
-
-            }, 1000)
-        }
+      if (revealTop < windowHeight) {
+          setTimeout(() => {
+          setIsAnimateds(true);
+          }, 1000)
       }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
-  
-    useEffect(() => {
+  }, []);
 
-      window.addEventListener("scroll", handleScroll);
-
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }, []);
 
 
 
@@ -51,34 +49,33 @@ function Seminars() {
                 <p>
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                 </p>
-
+            </div>
 
     <div className='seminars_body_mid'>
-                <div className="reveal-container">
-      <div
-        ref={revealRef}
-        className={`seminar_reveal-content ${isVisible ? "seminar_visible" : ""}`}
-      >
-        <div className="sized_svg">
-<SeminarIcon />
-        </div>
-      </div>
+    <div className="reveal-container">
+<div className='s_inner_decoy'>
+            <div className='s_decoy'>
+            </div>
+            <div id='movingDiv'>
+    <div className={isAnimateds ? "s_visible" : "reveal-content"}><SeminarIcon /></div>
+            </div>
     </div>
+</div>
 
            <div className='seminars_list'>
+           <div ref={revealRef1}>
+          </div>
                 <ul>
                     <h3>No Workshops Scheduled Currently</h3>
                     <p>They will resume after Spring</p>
                 </ul>
             </div>
-
-
-
             </div>
+      
 
   <div className='seminar_pic_section'>
   </div>
-</div>
+
             <img alt='' src={seminar_pic} className='seminar_pic'/>
             <div className='seminars_body_bottom'>
                 <h1> Sign Up For the Next Workshop Here</h1>
