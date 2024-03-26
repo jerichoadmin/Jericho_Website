7819c73369c516fb2de8394985bf18f9c5b77158
Josh Hobson <joshuahobson1995@gmail.com>
2024-03-26 11:55:43 -0600

    popup

--- src/components/Home/Popup/Popup.js
+++ src/components/Home/Popup/Popup.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import './Popup.css'
+import React, { useState } from 'react';
+import './Popup.css';
 
 function Popup() {
+  const [isVisible, setIsVisible] = useState(true);
 
+  const handleClose = () => {
+    setIsVisible(false);
+  };
 
   return (
-    <div className='popup_outer_shell'>
-
-        <div>
-            <img />
-            <span class="material-symbols-outlined">
-                warning
+    isVisible && (
+      <div className='popup_outer_shell'>
+        <div className='popup_dark_background'>
+          <div className='popup_inner'>
+            <span className="material-symbols-outlined">
+              warning
             </span>
-            <h1>Hey! We are having some technical issues</h1>
+            <h1>Hey!</h1>
+            <h1> We are having some technical issues</h1>
             <p>Our hosting provider is experiencing some downtime. The website is functional, but you can't see our images or blogs at the moment </p>
+
+            <button onClick={handleClose}>Close</button>
+          </div>
         </div>
-
-
-    </div>
-  )
+      </div>
+    )
+  );
 }
 
-export default Popup
+export default Popup;
