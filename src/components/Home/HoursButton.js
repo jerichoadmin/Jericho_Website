b39db179f8eca0198743579ddc444503ab23eb4d
Josh Hobson <111313914+joshhobson95@users.noreply.github.com>
2024-10-30 20:08:59 -0600

    Update HoursButton.js
    Update Hours Mobile

--- src/components/Home/HoursButton.js
+++ src/components/Home/HoursButton.js
@@ -30,7 +30,7 @@ const HoursButton = () => {
           className='modal'
         >
           <button onClick={closeModal}>Close </button>
-          <p>Hours: Monday-Saturday 9-5 | Sundays 11-5</p>
+          <p>Hours: Monday-Saturday 9-5 | Sundays 11-4</p>
         </dialog>
       )}
     </div>
